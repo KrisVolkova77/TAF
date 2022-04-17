57646c7eb3320df4fa99529840cd6771af3af54b
KrisVolkova77 <102957139+KrisVolkova77@users.noreply.github.com>
2022-04-17 21:18:39 +0300

    homework

--- src/main/java/dz2.java
+++ src/main/java/dz2.java
@@ -1,6 +1,9 @@
 public class dz2 {
-    public static void div(int a, int b) {
-        int c = a * b;
+    public static int[] div(int a , int b) {
+        a = 20;
+        b = 2;
+        int c = 20 * 2;
         System.out.println(c);
+        return new int[0];
     }
 }
--- src/test/java/dz2test.java
+++ src/test/java/dz2test.java
@@ -4,38 +4,43 @@ import org.testng.annotations.Test;
 
 import static java.lang.Thread.sleep;
 
-public class dz1test {
+public class dz2test {
+
     @Test
-    public void testdiv() {
-        Assert.assertEquals(dz1.div(10, 2), 5, "Верное...");
+    public void testdiv() {Assert.assertEquals(dz2.div(20, 2), 40, "OK...");
     }
 
     @Test(enabled = false)
-    public void testdiv1() {
-        Assert.assertEquals(dz1.div(10, 2), 5, "Верное...");
+    public void testdiv1() {Assert.assertEquals(dz2.div(20, 2), 40, "OK...");
     }
 
     @Test(description = "Test with description")
     public void testdiv2() {
-        Assert.assertEquals(dz1.div(10, 2), 5, "Верное...");
+        Assert.assertEquals(dz2.div(20, 2), 40, "OK...");
     }
+
 
     @Test(testName = "Test with name")
     public void testdiv3() throws InterruptedException {
-        Assert.assertEquals(dz1.div(10, 2), 5, "Верное...");}
+        Assert.assertEquals(dz2.div(20, 2), 40, "OK...");
+    }
 
     @Test(timeOut = 2000)
     public void waitLongTimeTest() throws InterruptedException {
-        sleep(1000);}
+        sleep(1000);
+    }
 
-    @Test(invocationCount = 4, invocationTimeOut = 2000, threadPoolSize = 4)
+    @Test(invocationCount = 4, invocationTimeOut = 1000, threadPoolSize = 4)
     public void invocationCountTest() throws InterruptedException {
         sleep(500);
-        Assert.assertEquals(dz1.div(10, 2), 5, "Верное...");}
+        Assert.assertEquals(dz2.div(20, 2), 40, "OK...");
+    }
 
     @Test(dataProvider = "dataForDiv", dataProviderClass = StaticProvider.class)
     public void testDataProvider(int a, int b, int expectedResult) {
-        Assert.assertEquals(dz1.div(a, b), expectedResult, "Верное...");}
+        Assert.assertEquals(dz2.div(a, b), expectedResult, "OK...");
+    }
 }
+
 
 
