554805e5b8dca2d68f758d6089914ba1c3aedcba
KrisVolkova77 <102957139+KrisVolkova77@users.noreply.github.com>
2022-06-26 13:53:41 +0300

    home

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -1,13 +1,39 @@
 package tests;
 
+import Confuguration.ReadProperties;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class LoginTest {
 
+    private Object loginPage;
+
+    private WebElement loginPage(String jhkhllmf, String password) {
+        return null;
+    }
+
+
     @Test(description = "Правильный логин")
     public void testName() {
         Assert.assertTrue(true);
+    }
+
+
+    @Test(description = "Неправильный емайл")
+    public void incorrectPsw() {
+        Assert.assertEquals(
+                loginPage("jhkhllmf", ReadProperties.password()).getText(),
+                "Your email or password is wrong");
+    }
+
+
+
+    @Test(description = "Неверный пароль")
+    public void incorrectEmail() {
+        Assert.assertEquals(
+                loginPage(ReadProperties.username(), "1245h").getText(),
+                "Your email or password is wrong");
     }
 }
 
