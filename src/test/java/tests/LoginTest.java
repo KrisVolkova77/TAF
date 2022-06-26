8930dfea73e4431c3edf384e446e5a803682d61f
KrisVolkova77 <102957139+KrisVolkova77@users.noreply.github.com>
2022-06-26 13:21:24 +0300

    home

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -1,4 +1,15 @@
 package tests;
 
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class LoginTest {
+
+    @Test(description = "Правильный логин")
+    public void testName() {
+        Assert.assertTrue(true);
+    }
 }
+
+
+
